8cd4d565893c9ee923932472077be8052a082f59
nstuhldreher1 <nbstuhldreher@gmail.com>
2026-03-16 18:48:06 -0400

    additional webpack configs

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -5,28 +5,88 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: "/"
-  },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist")
-    },
-    port: 8080,
-    hot: true,
-    open: true,
-    devMiddleware: {
-      writeToDisk: true
+export default (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  if (isProd) {
+    return {
+      entry: './src/index.js',
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
+      },
+      mode: "production",
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        })
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader']
+          }
+        ]
+      }
+    };
+
+  }else{
+    return{
+      entry: './src/index.js',
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
+      },
+      mode: "development",
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        })
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader']
+          }
+        ]
+      },
+      devServer: {
+        static: {
+          directory: path.resolve(__dirname, "dist")
+        },
+        port: 8080,
+        hot: true,
+        open: true,
+        devMiddleware: {
+          writeToDisk: true
+        }
+      }
     }
   }
 };
